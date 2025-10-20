caeadb514cffeb4c21c3600c2205feba747b9b4f
SPPBogor <penjualbahanbangunan@gmail.com>
2025-10-20 14:29:35 +0700

    Delete main.py

--- main.py
+++
@@ -1,6 +0,0 @@
-# main.py
-from app import app  # pastikan di app.py ada baris: app = Flask(__name__)
-
-# Opsional: untuk debug di lokal
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
